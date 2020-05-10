ea8044b415cf6026d0c17fbede1ccac3024e7a95
yangjinan <1269719239@qq.com>
2020-05-10 20:45:09 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/sdwu/android02/homework/sn170507180218/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.sdwu.android02.homework.sn170507180218;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
